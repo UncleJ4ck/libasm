12d5b842417dbde61bdfc4ea3051757dd8e6b46d
Sc4r3Cr0w <52085661+UncleJ4ck@users.noreply.github.com>
2022-12-16 21:48:49 +0100

    fix: "style and stuff"

--- srcs/ft_strdup.s
+++ srcs/ft_strdup.s
@@ -7,19 +7,19 @@ section .text
 ; RDI == src
 ft_strdup:
 	push	rbx
-	sub		rsp, 8 ; pushing rbx and aligning the stack and place for rbx
+	sub	rsp, 8 ; pushing rbx and aligning the stack and place for rbx
 	push	rdi
 	call	ft_strlen ; calculating the source length
-	mov		rdi, rax ; moving the result to rdi to use it in malloc
+	mov	rdi, rax ; moving the result to rdi to use it in malloc
 	call	malloc WRT ..plt 
-	pop		rdi
-	cmp		rax, 0 ; check if the source is null
-	jz		return
-	mov		rsi, rdi ; moving the source and destination to ft_strcpy arguments 
-	mov		rdi, rax
+	pop	rdi
+	cmp	rax, 0 ; check if the source is null
+	jz	return
+	mov	rsi, rdi ; moving the source and destination to ft_strcpy arguments 
+	mov	rdi, rax
 	call	ft_strcpy 
 
 return:
-	add		rsp, 8 ; returning the stack as if it was before pushing rbx
-	pop		rbx
+	add	rsp, 8 ; returning the stack as if it was before pushing rbx
+	pop	rbx
 	ret
